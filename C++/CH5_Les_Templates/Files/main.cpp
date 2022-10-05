912fc03f8caf48ee930314a0488c1c60aca5dd72
Clément Daguenet <99010993+ClementDaguenet@users.noreply.github.com>
2022-10-05 11:18:07 +0200

    Update Files

--- C++/CH5_Les_Templates/Files/main.cpp
+++ C++/CH5_Les_Templates/Files/main.cpp
@@ -2,30 +2,33 @@
 
 int main()
 {
-  int taille = 0;
-  cout << "Quelle taille de file ? : ";
-  cin >> taille;
+  int tailleE = 0;
+  int tailleC = 0;
+  cout << "Quelle taille de file d'entiers ? : ";
+  cin >> tailleE;
+  cout << "Quelle taille de file de caractères ? : ";
+  cin >> tailleC;
   system("clear");
 
   cout << "Voici une file d'entiers : " << endl;
-  File<int> file1(taille);
-  for (int indice = 0; indice < taille; indice++)
+  File<int> file1(tailleE);
+  for (int indice = 0; indice < tailleE; indice++)
     {
       file1.Enfiler(indice);
     }
-  for (int indice = 0; indice < taille; indice++)
+  for (int indice = 0; indice < tailleE; indice++)
     {
       cout << file1.Defiler() << " ";
     }
   cout << endl;
 
   cout << endl << "Voici une file de caractères : " << endl;
-  File<char> file2(taille);
-  for (int indice = 0; indice < taille; indice++)
+  File<char> file2(tailleC);
+  for (int indice = 0; indice < tailleC; indice++)
     {
       file2.Enfiler('A' + static_cast<char>(indice));
     }
-  for (int indice = 0; indice < taille ;indice++)
+  for (int indice = 0; indice < tailleC ;indice++)
     {
       cout << file2.Defiler() << " ";
     }
